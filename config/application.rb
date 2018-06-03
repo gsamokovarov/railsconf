c49b9ff2592d65c4e162a1304b2aa89e26474ecd
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-06-03 14:35:56 +0900

    Don't load active record

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require "active_model/railtie"
 require "active_job/railtie"
-require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
